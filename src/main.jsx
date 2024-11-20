9e3881f2a4168b8567d583a51963432aa411b116
GenZonix-Lab <solutiongenzonix@gmail.com>
2024-11-20 23:45:40 +0530

    update responsive

--- src/main.jsx
+++ src/main.jsx
@@ -8,22 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
-
-//slide4_content
-const slide4_h1=document.getElementById("slide4_head1")
-const slide4_h2=document.getElementById("slide4_head2")
-const slide4_h3=document.getElementById("slide4_head3")
-const slide4_p1=document.getElementById("slide4_para1")
-const slide4_p2=document.getElementById("slide4_para2")
-const slide4_p3=document.getElementById("slide4_para3")
-
-slide4_h1.innerText="Innovative, Hands-On Learning Kits"
-slide4_h2.innerText="Expert Support & Resources"
-slide4_h3.innerText="Affordable & Curated for Value"
-slide4_p1.innerText="Genzonix offers high-quality STEM and IoT DIY kits designed to engage learners through hands-on activities. Each kit fosters creativity, problem-solving, and technical skills, making learning practical and fun for all ages."
-slide4_p2.innerText="Genzonix provides easy-to-follow guides, tutorials, and customer support, ensuring even beginners can dive into STEM projects confidently. We aim to make tech learning accessible and enjoyable, regardless of experience level."
-slide4_p3.innerText="Our kits are competitively priced, balancing affordability with quality. We thoughtfully curate each kit to include essential components, giving customers a comprehensive, high-value experience without breaking the bank."
-
-
-
-console.log("connected to Genzonix")
