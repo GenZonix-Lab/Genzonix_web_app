a21ce2a11d8dea8fcbb7d2335c4d4b2546eef70e
GenZonix-Lab <solutiongenzonix@gmail.com>
2024-11-26 11:52:05 +0530

    JS file move to public

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Script from '../script'
+import Script from '../public/script'
 
 function App() {
   const [count, setCount] = useState(0)
