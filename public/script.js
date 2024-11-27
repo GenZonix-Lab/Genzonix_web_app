c0f17ca3545c0fc830ecac4aa145dc601e0d8e73
GenZonix-Lab <solutiongenzonix@gmail.com>
2024-11-27 15:43:00 +0530

    project 4 and 5 done.

--- public/script.js
+++ public/script.js
@@ -5,6 +5,8 @@ let iot_array=[
     {project:'CHANGE LED COLOR AND ON/OFF LED THROUGH BLUETOOTH',url: 'gzxproj24/GZXP24001.html'},
     {project:'SMART PARKING EMAIL-NOTIFICATION SYSTEM',url: 'gzxproj24/GZXP24002.html'},
     {project:'SMART SECURITY INDICATION AND EMAIL NOTIFICATION SYSTEM',url: 'gzxproj24/GZXP24003.html'},
+    {project:'SMART HOME AUTOMATION SYSTEM',url: 'gzxproj24/GZXP24004.html'},
+    {project:'VOICE COMMAND THROUGH HOME AUTOMATION SYSTEM',url: 'gzxproj24/GZXP24005.html'},
 ]
 const data=document.getElementById('iotlist')
 iot_array.forEach(items => {
