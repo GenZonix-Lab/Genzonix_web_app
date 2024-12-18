6d9208e0b2c9dee8dd0c1492ba273379e294b845
GenZonix-Lab <solutiongenzonix@gmail.com>
2024-12-18 21:02:26 +0530

    Iot Scripts added

---
+++ public/iot.js
@@ -0,0 +1,19 @@
+//IoT Project list
+const iotlist = document.getElementById("iotlist")
+
+let iot_array=[
+    {project:'CHANGE LED COLOR AND ON/OFF LED THROUGH BLUETOOTH',url: 'gzxproj24/GZXP24001.html'},
+    {project:'SMART PARKING EMAIL-NOTIFICATION SYSTEM',url: 'gzxproj24/GZXP24002.html'},
+    {project:'SMART SECURITY INDICATION AND EMAIL NOTIFICATION SYSTEM',url: 'gzxproj24/GZXP24003.html'},
+    {project:'SMART HOME AUTOMATION SYSTEM',url: 'gzxproj24/GZXP24004.html'},
+    {project:'VOICE COMMAND THROUGH HOME AUTOMATION SYSTEM',url: 'gzxproj24/GZXP24005.html'},
+]
+const list_data=document.getElementById('iotlist')
+iot_array.forEach(items => {
+    const listItem = document.createElement('li');
+    const link = document.createElement('a');
+    link.href=items.url
+    link.textContent=items.project;
+    listItem.appendChild(link);
+    list_data.appendChild(listItem);
+});
--- public/script.js
+++ public/script.js
@@ -16,25 +16,3 @@ slide4_p3.innerText = "Our kits are competitively priced, balancing affordabilit
 
 
 console.log("connected to Genzonix") 
-
-
-
-//IoT Project list
-const iotlist = document.getElementById("iotlist")
-
-let iot_array=[
-    {project:'CHANGE LED COLOR AND ON/OFF LED THROUGH BLUETOOTH',url: 'gzxproj24/GZXP24001.html'},
-    {project:'SMART PARKING EMAIL-NOTIFICATION SYSTEM',url: 'gzxproj24/GZXP24002.html'},
-    {project:'SMART SECURITY INDICATION AND EMAIL NOTIFICATION SYSTEM',url: 'gzxproj24/GZXP24003.html'},
-    {project:'SMART HOME AUTOMATION SYSTEM',url: 'gzxproj24/GZXP24004.html'},
-    {project:'VOICE COMMAND THROUGH HOME AUTOMATION SYSTEM',url: 'gzxproj24/GZXP24005.html'},
-]
-const list_data=document.getElementById('iotlist')
-iot_array.forEach(items => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
-    link.href=items.url
-    link.textContent=items.project;
-    listItem.appendChild(link);
-    list_data.appendChild(listItem);
-});
